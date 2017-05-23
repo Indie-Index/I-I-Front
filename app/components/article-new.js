c26d21ee3578c561e9198cad133c54a17332a58b
dsweetser <daniel.sweetser@gmail.com>
2017-05-23 16:11:32 -0400

    Create new article route and component
    Created a route and component under otto for new articles
    
    Next step will be to get the model hook into these

---
+++ app/components/article-new.js
@@ -0,0 +1,12 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  actions: {
+    save() {
+      this.sendAction('save', this.get('article'));
+    },
+    cancel() {
+      this.sendAction('cancel');
+    },
+  },
+});
